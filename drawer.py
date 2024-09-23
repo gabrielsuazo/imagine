d09b6b8b558cb3419601f0719d46f7661ee7a5cc
gabri <gabrielsuazo7@gmail.com>
2024-09-23 15:52:56 +0200

    [DEV] Implement uses of analyser into drawer functions, still in progress

--- drawer.py
+++ drawer.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import analyser
 
 
 def draw_new_line(line: str):
@@ -11,25 +12,27 @@ def draw_new_line(line: str):
     # determine the sentiment score of each word and of the sentence itself
     score = get_line_score(line)
     score_words = []
-    for word in line:
+    words_list = analyser.tokenize(line)
+    for word in words_list:
         score_words.append(get_word_score(word))
 
     # determine the colors of each word (depends on neighbor words)
     colors = []
-    for word in line:
+    for word in words_list:
         colors.append(get_word_color(word))
 
+    # proportional to the norm of the compound
     width = ""
 
-    draw_words(t, line, score_words, colors)
+    draw_words(t, words_list, score_words, colors)
 
 
 def get_line_score(line: str):
-    return random.uniform(0, 1)
+    return analyser.analyze(line)['compound']
 
 
 def get_word_score(word: str):
-    return random.uniform(0, 1)
+    return analyser.analyze(word)['compound']
 
 
 def get_word_color(word: str):
@@ -46,10 +49,10 @@ def draw_word(t: turtle.Turtle, word, previous_score, new_score, color):
     t.forward(distance)
 
 
-def draw_words(t: turtle.Turtle, line, scores, colors):
+def draw_words(t: turtle.Turtle, word_list, scores, colors):
     previous_score = 0
-    for i in range(len(line)):
-        word = line[i]
+    for i in range(len(word_list)):
+        word = word_list[i]
         new_score = scores[i]
         new_color = colors[i]
         draw_word(t, word, previous_score, new_score, new_color)
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import analyser
 import drawer
+import os
+import reader
 
 
-def main():
-    drawer.draw_new_line("dsqkljdlsqk")
-    drawer.draw_new_line("dsqjkldjqs")
-    drawer.draw_new_line("sdqdsq")
-    drawer.draw_new_line("dsqkljdsqdsqlsqk")
-    drawer.draw_new_line("dsef")
+def main(text_path: str, save_image: bool = True):
+    if not os.path.isfile(text_path):
+        print(text_path + " is not a valid path. Please enter a valid path and try again")
+        return
+    analyser.initialisation()
+    lines = reader.read_lines(text_path)
+    for line in lines:
+        drawer.draw_new_line(line)
 
 
 if __name__ == '__main__':
-    main()
+    main('./examples/texts/the_raven.txt')
